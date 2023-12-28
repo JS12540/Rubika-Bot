0253c2c8f67bc42a1fb95914af1db2b6a16ad43d
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-28 18:12:15 +0330

    Create str/main and edit this

---
+++ src/main.py
@@ -0,0 +1,232 @@
+from pyrubi import Client
+from requests import get
+from langdetect import detect
+from random import choice
+from re import sub
+
+token = '295809:6517005fc9455'
+# personal token to perform translation operations
+# to get your personal token, visit one-api.ir
+
+
+def main():
+    client = Client(session='session', time_out=10)
+    for update in client.on_message(filters=['Channel', 'User']):
+        _me = client.get_me()['user']['user_guid']
+        _message_id = client.get_messages_by_id(
+            object_guid=update.object_guid,
+            message_ids=[update.message_id]
+        )
+        try:
+            _guid_message = client.get_messages_by_id(
+                object_guid=update.object_guid,
+                message_ids=[_message_id['messages'][0]['reply_to_message_id']]
+            )
+            if _me == _guid_message['messages'][0]['author_object_guid']:
+                # Using this newly added feature,
+                # you can get your answer just by replying to one of the bot's messages!
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='__Please wait...__',
+                    message_id=update.message_id
+                )
+                responce = get(f'https://chatgpt-api3.onrender.com?text={update.text}').json()
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text=responce['message'],
+                    message_id=update.message_id
+                )
+
+        except KeyError:
+            pass
+
+            if update.text.startswith('+'):
+                if update.text == '+':
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='Please Enter a text\nExample: __+hello__'
+                    )
+                else:
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='__Please wait...__',
+                        message_id=update.message_id
+                    )
+                    responce = get(f'https://chatgpt-api3.onrender.com?text={update.text}').json()
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text=responce['message'],
+                        message_id=update.message_id
+                    )
+
+
+            elif update.text.startswith('img'):
+                if update.text == 'img':
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='Please Enter a text\nExample: __img amir__',
+                        message_id=update.message_id
+                    )
+                else:
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='__Please wait...__',
+                        message_id=update.message_id
+                    )
+                    text = sub('img', '', update.text).strip()
+                    request = get(f'https://haji-api.ir/prompts/?text={text}').json()
+                    responce = get(choice(request['result']))
+                    with open('.img.png', 'wb') as _file:
+                        _file.write(responce.content)
+
+                    client.send_image(
+                        object_guid=update.object_guid,
+                        file='.img.png',
+                        text=f'your image is realy👍\ncontent:\"{text}\"\ndeveloper: @activate_sh',
+                        message_id = update.message_id,
+                    )
+
+
+            elif update.text.startswith('logo'):
+                if update.text == 'logo':
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='Please Enter a text\nExample: __logo amir__',
+                        message_id=update.message_id
+                    )
+                else:
+                    pass
+
+
+            elif update.text == '$' or update.text == 'usd':
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='__Please wait...__',
+                    message_id=update.message_id
+                )
+                responce = get('https://pyrubi.b80.xyz/usd.php').json()
+                responce_ = responce['result']['Us Dollar']
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='**Us Dollar:**\n\nAverage: __%s__\nMax: __%s__\nMin: __%s__' % (
+                        responce_['Average'], responce_['Max'], responce_['Min']
+                    ),
+                    message_id=update.message_id
+                )
+
+
+            elif update.text.startswith('font'):
+                if update.text == 'font':
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='Please Enter a text\nExample: __font amir__',
+                        message_id=update.message_id
+                    )
+                else:
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='__Please wait...__',
+                        message_id=update.message_id
+                    )
+                    text = update.text.split('font')[-1]
+                    if detect(text) in ['fa', 'ar', 'ur']:
+                        request = get(f'http://api.codebazan.ir/font/?type=fa&text={text}').json()
+                    else:
+                        request = get(f'http://api.codebazan.ir/font/?text={text}').json()
+
+                    results = client.get_messages_by_id(
+                        object_guid=update.object_guid,
+                        message_ids=[update.message_id]
+                    )
+                    user_guid = results['messages'][0]['author_object_guid']
+                    for num in range(1, 10):
+                        try:
+                            client.send_text(
+                                object_guid=user_guid,
+                                text=request['result'][str(num)],
+                                message_id=update.message_id
+                            )
+                        except KeyError:
+                            client.send_text(
+                                object_guid=user_guid,
+                                text=request['Result'][str(num)],
+                                message_id=update.message_id
+                            )
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='The fonts have been sent to your PM',
+                        message_id=update.message_id
+                    )
+
+
+            elif update.text == 'news':
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='__Please wait...__',
+                    message_id=update.message_id
+                )
+                _responce = get(f'https://one-api.ir/rss/?token={token}&action=irinn').json()
+                responce = choice(_responce['result']['item'])
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='%s\n\n%s\n\n%s' % (
+                        responce['title'], responce['description'], responce['pubDate']
+                    ),
+                    message_id=update.message_id
+                )
+
+
+            elif update.text == 'joke':
+                responce = get(f'https://one-api.ir/joke/?token={token}').json()
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text=responce['result'],
+                    message_id=update.message_id
+                )
+
+
+            elif update.text == 'link':
+                info = client.get_chat_info(object_guid=update.object_guid)
+                try:
+                    _results = client.get_link(object_guid=info['group']['group_guid'])
+                    group_link = _results['join_link']
+                    group_title = info['group']['group_title']
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text=f'The link to the {group_title} group is:\n\n``{group_link}``'
+                    )
+                except:
+                    client.send_text(
+                        object_guid=update.object_guid,
+                        text='**Please admin the robot account in the group**',
+                        message_id=update.message_id
+                    )
+
+
+            elif update.text == 'test':
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text='The bot is active',
+                    message_id=update.message_id
+                )
+
+
+            elif update.text == 'time':
+                responce = get('http://api.codebazan.ir/time-date/?td=all')
+                client.send_text(
+                    object_guid=update.object_guid,
+                    text=responce.text,
+                    message_id=update.message_id
+                )
+
+
+        except TimeoutError:
+            client.send_text(
+                object_guid=update.object_guid,
+                text='timeout-err',
+                message_id=update.message_id
+            )
+
+
+if __name__ == '__main__':
+    main()
