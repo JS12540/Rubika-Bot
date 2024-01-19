40b560e962a50f42bf4b7461e214400b3e140181
amirali irvany <dev.amirali.irvany@gmail.com>
2024-01-19 14:10:40 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from langdetect import detect
 from subprocess import getoutput
 from random import choice, randint
 from requests import get, exceptions
+from better_profanity import profanity
 
 # Enter the guid of your group(s) into this list
 groups = [
@@ -17,24 +18,21 @@ token = '295809:6517005fc9455'
 
 def main():
 
-    TIMEOUT = 10
-    client = Client(session='session', time_out=TIMEOUT)
+    CAPABILITIES = ['img', 'font', 'ai']
+    client = Client(session='session', time_out=10)
     for guid in groups:
         if guid.strip() == '':
             groups.remove(guid)
 
-        # join_status = client.join_chat(guid_or_link=guid)
-        try:
-            result = client.get_chat_info(object_guid=guid)
-            group_name = result['group']['group_title']
-            client.send_text(
-                object_guid=guid,
-                text=f'The bot was successfully activated in the {group_name} group'
-            )
-        except Exception as err:
-            print(err)
+        admins = client.get_all_members(object_guid=guid, just_get_guids=True)
+        result = client.get_chat_info(object_guid=guid)
+        group_name = result['group']['group_title']
+        client.send_text(
+            object_guid=guid,
+            text=f'The bot was successfully activated in the \'{group_name}\''
+        )
 
-    for update in client.on_message(filters=['Channel', 'User']):
+    for update in client.on_message(filters=['User', 'Channel']):
         if update.object_guid in groups:
             _me = client.get_me()['user']['user_guid']
             _message_id = client.get_messages_by_id(
@@ -46,12 +44,12 @@ def main():
                     object_guid=update.object_guid,
                     message_ids=[_message_id['messages'][0]['reply_to_message_id']]
                 )
-                if _me == _guid_message['messages'][0]['author_object_guid']:
-                    # Using this newly added feature,
-                    # you can get your answer just by replying to one of the bot's messages!
+                if (
+                    ('ai' in CAPABILITIES) and _me == _guid_message['messages'][0]['author_object_guid']
+                ):
                     client.send_text(
                         object_guid=update.object_guid,
-                        text='__Please wait...__',
+                        text='Processing, __please wait..__',
                         message_id=update.message_id
                     )
                     responce = get(f'https://pyrubi.b80.xyz/chat.php?text={update.text}').json()
@@ -60,11 +58,11 @@ def main():
                         text=responce[0]['text'],
                         message_id=update.message_id
                     )
-            except KeyError:
+            except Exception:
                 pass
 
             try:
-                if update.text.startswith('+'):
+                if update.text.startswith('+') and ('ai' in CAPABILITIES):
                     if update.text.strip() == '+':
                         client.send_text(
                             object_guid=update.object_guid,
@@ -73,7 +71,7 @@ def main():
                     else:
                         client.send_text(
                             object_guid=update.object_guid,
-                            text='__Please wait...__',
+                            text='Processing, __please wait..__',
                             message_id=update.message_id
                         )
                         responce = get(f'https://pyrubi.b80.xyz/chat.php?text={update.text[1:]}').json()
@@ -84,78 +82,55 @@ def main():
                         )
 
 
-                elif update.text.startswith('voice'):
-                    if update.text.strip() == 'voice':
-                        client.send_text(
-                            object_guid=update.object_guid,
-                            text='Please enter a text\nExample: __voice man hello__',
-                            message_id=update.message_id
-                        )
-                    else:
-                        client.send_text(
-                            object_guid=update.object_guid,
-                            text='__Please wait...__',
-                            message_id=update.message_id
-                        )
-                        mod = update.text.split()[1].strip()
-                        text = update.text.split()[-1].strip()
-                        try:
-                            request = get(f'https://pyrubi.b80.xyz/voice.php?text={text}&mod={mod}').json()
-                            responce = get(request['result'])
-                            with open('.voice.mp3', 'wb') as file_:
-                                file_.write(responce.content)
-
-                            client.send_voice(
-                                object_guid=update.object_guid,
-                                file='.voice.mp3',
-                                message_id=update.message_id,
-                                text=f'your voice is realy👍\ncontent:\"{text}\"\ndeveloper: @slash_dev'
-                            )
-                        except exceptions.JSONDecodeError:
-                            client.send_text(
-                                object_guid=update.object_guid,
-                                text='❌ Problem!\nThis is your problem\nPlease read the guide again',
-                                message_id=update.message_id
-                            )
-                        except:
-                            client.send_text(
-                                object_guid=update.object_guid,
-                                text='❌ Problem!\nThis is our problem\nPlease try again',
-                                message_id=update.message_id
-                            )
-
-
-                elif update.text.startswith('img'):
+                elif update.text.startswith('img') and ('img' in CAPABILITIES):
                     if update.text.strip() == 'img':
                         client.send_text(
                             object_guid=update.object_guid,
                             text='Please Enter a text\nExample: __img wolf__',
                             message_id=update.message_id
                         )
+                    elif profanity.contains_profanity(update.text):
+                        client.send_text(
+                            object_guid=update.object_guid,
+                            text='**Your message has been detected as obscene**',
+                            message_id=update.message_id
+                        )
                     else:
                         client.send_text(
                             object_guid=update.object_guid,
-                            text='__Please wait...__',
+                            text='Processing, __please wait..__',
                             message_id=update.message_id
                         )
                         text = sub('img', '', update.text).strip()
-                        request = get(f'https://haji-api.ir/prompts/?text={text}').json()
-                        responce = get(choice(request['result']))
-                        with open('.img.png', 'wb') as _file:
-                            _file.write(responce.content)
+                        try:
+                            if detect(text) in ['fa', 'ar', 'ur']:
+                                request_text = get(
+                                    f'https://one-api.ir/translate/?token={token}&action=google&lang=fa&q={text}').json()
 
-                        client.send_image(
-                            object_guid=update.object_guid,
-                            file='.img.png',
-                            text=f'your image is realy👍\ncontent:\"{text}\"\ndeveloper: @slash_dev',
-                            message_id = update.message_id,
-                        )
+                            _text = request_text['result']
+                            request = get(f'https://haji-api.ir/prompts/?text={_text}').json()
+                            responce = get(choice(request['result']))
+                            with open('.img.png', 'wb') as _file:
+                                _file.write(responce.content)
+
+                            client.send_image(
+                                object_guid=update.object_guid,
+                                file='.img.png',
+                                text=f'your image is realy👍\ncontent:\"{_text}\"\ndeveloper: @slash_dev',
+                                message_id = update.message_id,
+                            )
+                        except Exception:
+                            client.send_text(
+                                object_guid=update.object_guid,
+                                text='**There was a problem with us!. Please try again**',
+                                message_id=update.message_id
+                            )
 
 
                 elif update.text == '$' or update.text == 'usd':
                     client.send_text(
                         object_guid=update.object_guid,
-                        text='__Please wait...__',
+                        text='Processing, __please wait..__',
                         message_id=update.message_id
                     )
                     responce = get(f'https://one-api.ir/price/?token={token}').json()
@@ -173,7 +148,7 @@ def main():
                     )
 
 
-                elif update.text.startswith('font'):
+                elif update.text.startswith('font') and ('font' in CAPABILITIES):
                     if update.text == 'font':
                         client.send_text(
                             object_guid=update.object_guid,
@@ -183,11 +158,17 @@ def main():
                     else:
                         client.send_text(
                             object_guid=update.object_guid,
-                            text='__Please wait...__',
+                            text='Processing, __please wait..__',
                             message_id=update.message_id
                         )
                         _text = update.text.split('font')[-1]
-                        if detect(text) in ['fa', 'ar', 'ur']:
+                        if profanity.contains_profanity(_text):
+                            client.send_text(
+                                object_guid=update.object_guid,
+                                text='',
+                                message_id=update.message_id
+                            )
+                        elif detect(text) in ['fa', 'ar', 'ur']:
                             request = get(f'http://api.codebazan.ir/font/?type=fa&text={_text}').json()
                         else:
                             request = get(f'http://api.codebazan.ir/font/?text={_text}').json()
@@ -212,7 +193,7 @@ def main():
                                 )
                         client.send_text(
                             object_guid=update.object_guid,
-                            text='The fonts have been sent to your PV',
+                            text='The fonts have been sent to your PV ✅',
                             message_id=update.message_id
                         )
 
@@ -220,7 +201,7 @@ def main():
                 elif update.text == 'news':
                     client.send_text(
                         object_guid=update.object_guid,
-                        text='__Please wait...__',
+                        text='Processing, __please wait..__',
                         message_id=update.message_id
                     )
                     _responce = get(f'https://one-api.ir/rss/?token={token}&action=irinn').json()
@@ -286,6 +267,55 @@ def main():
                         text=f'🌐 Ping now: **{str(results[0])}**',
                         message_id=update.message_id
                     )
+
+
+                elif update.author_guid in admins:
+                    if update.text.startswith('off'):
+                        if update.text.strip() == 'off':
+                            client.send_text(
+                                object_guid=update.object_guid,
+                                text='Please enter a text\nExample: __off ai__',
+                                message_id=update.message_id
+                            )
+                        else:
+                            value = update.text.split('off')[-1].strip()
+                            if value not in CAPABILITIES:
+                                client.send_text(
+                                    object_guid=update.object_guid,
+                                    text='**This feature is already disabled**',
+                                    message_id=update.message_id
+                                )
+                            else:
+                                CAPABILITIES.remove(value)
+                                client.send_text(
+                                    object_guid=update.object_guid,
+                                    text=f'Ability {value} Unreachable ✅',
+                                    message_id=update.message_id
+                                )
+
+
+                    elif update.text.startswith('on'):
+                        if update.text.strip() == 'on':
+                            client.send_text(
+                                object_guid=update.object_guid,
+                                text='Please enter a text\nExample: __on ai__',
+                                message_id=update.message_id
+                            )
+                        else:
+                            value = update.text.split('on')[-1].strip()
+                            if value in CAPABILITIES:
+                                client.send_text(
+                                    object_guid=update.object_guid,
+                                    text='**This feature is already enabled**',
+                                    message_id=update.message_id
+                                )
+                            else:
+                                CAPABILITIES.append(value)
+                                client.send_text(
+                                    object_guid=update.object_guid,
+                                    text=f'Ability {value} became accessible ✅',
+                                    message_id=update.message_id
+                                )
 
 
             except TimeoutError:
